e66d3f853e0889cf7e9bb772e2c96317d55831d7
Priyanka Babar <127826287+priyanka-babar@users.noreply.github.com>
2024-11-14 13:19:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+ import './App.css'
+import Weather from './Weather'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>hello priyanka</h2>
+      <Weather/>
+       
+    </>
+  )
+}
+
+export default App
